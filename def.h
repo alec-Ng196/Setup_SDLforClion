77162775ec1eae0ab64dc798aaccc71390885fa5
alec-Ng196 <23020601@vnu.edu.vn>
2024-04-14 13:35:24 +0700

    project2

---
+++ def.h
@@ -0,0 +1,14 @@
+//
+// Created by Admin on 14/4/2024.
+//
+
+#ifndef SDLWITHHEADER_DEF_H
+#define SDLWITHHEADER_DEF_H
+
+const int SCREEN_WIDTH = 800;
+const int SCREEN_HEIGHT = 600;
+const char* WINDOW_TITLE = "Test with many projects";
+
+
+
+#endif //SDLWITHHEADER_DEF_H
